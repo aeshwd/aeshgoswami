b0d83cb00b4421c0ffd6e9d008486cc6603bb919
aeshwd <aeshgoswamiwd@gmail.com>
2024-12-17 17:11:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+function showSidebar() {
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display= "block"
+}
+
+function hideSidebar(){
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none"
+}
+
+//Wrapper Section
+
+
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
+
+/* Places I Visited */
+
+const placesContainer = document.querySelector('.places-container');
+
+let scrollAmount = 0;
+
+function autoScroll() {
+    scrollAmount += 1;
+    if (scrollAmount >= placesContainer.scrollWidth - placesContainer.clientWidth) {
+        scrollAmount = 0;
+    }
+    placesContainer.scrollLeft = scrollAmount;
+}
+
+setInterval(autoScroll, 30);
+
+
+
+
+
+
+
+
+
